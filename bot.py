382de75777d91aa80ba9c832099f9e4ca949beb4
Vincent Carluer <vcarluer@gmail.com>
2023-12-24 16:55:54 +0100

    first command

--- bot.py
+++ bot.py
@@ -19,14 +19,14 @@ load_dotenv()
 logging.info("init discord client")
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents().all()
-# client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     logging.info(f'{bot.user} has connected to discord')
 
-# client.run(TOKEN)
-
+@bot.command(name="list")
+async def raptor_list(ctx):
+    await ctx.send("No list yet. Add one with '!hatch NAME'")
 
 bot.run(TOKEN)
