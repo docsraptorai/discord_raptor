743f1164efe36b1c20abab372553d0b2925fb8fa
Vincent Carluer <vcarluer@gmail.com>
2023-12-24 17:12:11 +0100

    move to slash command

--- bot.py
+++ bot.py
@@ -19,14 +19,21 @@ load_dotenv()
 logging.info("init discord client")
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
 
-@bot.event
+@client.event
 async def on_ready():
-    logging.info(f'{bot.user} has connected to discord')
+    # await tree.sync(guild=discord.Object(id=Your guild id))
+    logging.info(f'{client.user} has connected to discord')
+    for guild in client.guilds:
+        logging.info(f'Guild id:{guild.id}')
+        await tree.sync(guild=guild)
 
-@bot.command(name="list")
-async def raptor_list(ctx):
-    await ctx.send("No list yet. Add one with '!hatch NAME'")
+# , guild=discord.Object(id=12417128931)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name="list", description="list the raptors you have trained", guild=discord.Object(id=1138385140674998312))
+async def raptor_list(interaction):
+    await interaction.response.send_message("No list yet")
 
-bot.run(TOKEN)
+
+client.run(TOKEN)
